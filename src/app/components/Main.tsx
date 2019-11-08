7d1bd9beaed6e3a021be831f81db3b717385defb
Koha Choji <koha.choji@vistair.com>
2019-11-08 08:15:55 +0000

    Added navigation

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -4,11 +4,15 @@ import { store } from '../store'
 import { ConnectedDashboard } from "./Dashboard"
 import { Route, Router } from "react-router"
 import { history} from "../store/history"
+import { ConnectedNavigation } from "./Navigation"
 
 export const Main = () => (
     <Router history={history}>
         <Provider store={store}>
-            <Route exact path="/dashboard" render={()=>(<ConnectedDashboard/>)} />
+            <div>
+                <ConnectedNavigation/>
+                <Route exact path="/dashboard" render={()=>(<ConnectedDashboard/>)} />
+            </div>
         </Provider>
     </Router>
 )
