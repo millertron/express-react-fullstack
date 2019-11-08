0536cec1f6282043e8959507895167b87953bf1c
Koha Choji <koha.choji@vistair.com>
2019-11-08 08:10:52 +0000

    Added router

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from '../store'
 import { ConnectedDashboard } from "./Dashboard"
+import { Route, Router } from "react-router"
+import { history} from "../store/history"
 
 export const Main = () => (
-    <Provider store={store}>
-        <ConnectedDashboard />
-    </Provider>
+    <Router history={history}>
+        <Provider store={store}>
+            <Route exact path="/dashboard" render={()=>(<ConnectedDashboard/>)} />
+        </Provider>
+    </Router>
 )
